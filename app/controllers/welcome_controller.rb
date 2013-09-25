90afd3e2a2281439da5736957c0602c2e683e379
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 18:34:21 -0500

    placed the map on the view

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,10 @@
+class WelcomeController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def index
+    render action: :index, :layout => "application"
+  end
+
+end
