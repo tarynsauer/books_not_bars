c4aa62128bc409c95afc13ec8275a9657997a656
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-25 20:30:32 -0500

    Add dynamic  education and inmate data to view

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,4 +7,14 @@ class WelcomeController < ActionController::Base
     render action: :index, :layout => "application"
   end
 
+  def update
+    p params
+    edu_data_unit = DataUnit.where(state: params[:state], category: "education")
+    prison_data_unit = DataUnit.where(state: params[:state], category: "prison")
+    
+    p "X"*100
+    if request.xhr?
+     render json: { inmate_cost: prison_data_unit.first.data_num, pupil_cost: edu_data_unit.first.data_num}
+    end
+  end
 end
