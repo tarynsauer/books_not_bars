0153277abe08e025ae1a85845b5a394a683ab65a
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 17:21:31 -0500

    Add styles

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,7 +6,7 @@ var Map = function(selector){
     backgroundColor: 'none',
     color: '#f4f3f0',
     hoverColor: 'rgba(240,242,241,0.9)',
-    selectedColor: 'rgba(21,21,11,1)',
+    selectedColor: 'rgba(221,21,11,1)',
     scaleColors: ['#b6d6ff', '#005ace'],
     normalizeFunction: 'linear',
     enableZoom: false,
@@ -19,48 +19,50 @@ var Map = function(selector){
 
 // =================================================================
 
-  // var data_array = [];
-
-  // var w = 20,
-  //   h = 80;
-
-  // var x = d3.scale.linear()
-  //   .domain([0, 1])
-  //   .range([0, w]);
-
-  // var y = d3.scale.linear()
-  //   .domain([0, 100])
-  //   .rangeRound([0,h]);
-
-  // var chart = d3.select("#chart_holder").append("svg")
-  //   .attr("class", "chart")
-  //   .attr("width", w * data_array.length - 1)
-  //   .attr("height", h);
-
  
-  // var ChartTable = function(w, h){
-  //   this.w = w
-  //   this.h = h
+  var ChartTable = function(w, h, data_array){
+    this.w = w;
+    this.h = h;
+    this.data_array = data_array;
 
-  //   this.x = d3.scale.linear()
-  //     .domain([0, 1])
-  //     .range([0, w]);
+    this.x = d3.scale.linear()
+      .domain([0, 1])
+      .range([0, w]);
 
-  //   this.y = d3.scale.linear()
-  //     .domain([0, 100])
-  //     .rangeRound([0,h]);
-  // };
-
-  // Chart.prototype.buildChart = function(){
-  //   d3.select("#chart_holder").append("svg")
-  //     .attr("class", "chart")
-  //     .attr("width", this.w * data_array.length - 1)
-  //     .attr("height", this.h);    
-  // };
+    this.y = d3.scale.linear()
+      .domain([0, 100])
+      .rangeRound([0,h]);
+  };
 
 
+  ChartTable.prototype.render = function() {
+    var self = this
+    this.chart = d3.select("#chart_holder").append("svg")
+      .attr("class", "chart")
+      .attr("width", this.w * this.data_array.length - 1)
+      .attr("height", this.h);
+
+    this.bars = this.chart.selectAll("rect")
+      .data(this.data_array)
+      .enter().append("rect")
+      .attr("x", function(d, i) { return self.x(i) - .5; })
+      .attr("y", 100)
+      .attr("width", this.w)
+      .attr("height", this.y );
+  }
+
+  ChartTable.prototype.animateBars = function() {
+    var self = this;
+    this.bars.transition().attr("y",function(d) { return self.h - self.y(d) - .5; });
+  }
 
 
+  Map.prototype.getCoord = function() {
+    $(this.elem).on('click', function(event){
+      this.x_coord = event.pageX;
+      this.y_coord = event.pageY;
+    })
+  } 
 
   // chart.append("line")
   //    .attr("x1", 0)
@@ -75,38 +77,21 @@ var Map = function(selector){
 Map.prototype.statChange = function(){
   
   var self = this;
+  // var xCoord = this.getCoord();
+  // var yCoord = 
+
   $(this.elem).on('regionClick.jqvmap', function(event, code, region){
      $('#chart_holder').children().remove();
-     
       self.elem.attr('class', 'display');
       $('#close').show();
-      $('.stats').show();
+      $('.stats').show(); 
+
       $.post('/update',{ state: region }, function(response){
-    // =================================================================      
-        
-        var data_array = [response.pupil_cost/1000, response.inmate_cost/1000, 90]
-        var w = 20,
-          h = 80;
-        var x = d3.scale.linear()
-          .domain([0, 1])
-          .range([0, w]);
-        var y = d3.scale.linear()
-          .domain([0, 100])
-          .rangeRound([0,h]);
-        var chart = d3.select("#chart_holder").append("svg")
-          .attr("class", "chart")
-          .attr("width", w * data_array.length - 1)
-          .attr("height", h);
-        var bars = chart.selectAll("rect")
-          .data(data_array)
-          .enter().append("rect")
-          .attr("x", function(d, i) { return x(i) - .5; })
-          .attr("y", 80)
-          .attr("width", w)
-          .attr("height", y );
-     // =================================================================  
-          bars.transition().attr("y",function(d) { return h - y(d) - .5; });
-          self.assignStats(region, response.pupil_cost, response.inmate_cost)
+        var data_array = [response.pupil_cost/500, response.inmate_cost/500]        
+        var chart = new ChartTable(30, 200, data_array)
+        chart.render();
+        chart.animateBars();
+        self.assignStats(region, response.pupil_cost, response.inmate_cost)
       })
   }); 
 }
@@ -117,6 +102,7 @@ Map.prototype.assignStats = function(state, pupil_cost, inmate_cost){
   $('#inmate_cost').text(inmate_cost)
   $('#pupil_cost').text(pupil_cost)
 }
+
 
 
 $(document).ready(function() {
