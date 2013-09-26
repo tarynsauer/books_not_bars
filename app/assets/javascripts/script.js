b0778da42ece8d776eb5d7c0a9aec069002318b9
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-26 14:28:29 -0500

    Seed database

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -23,6 +23,7 @@ Map.prototype.statChange = function(){
       $('#close').show();
       $('.stats').show();
       $.post('/update',{ state: region }, function(response){
+        console.log(response);
         self.assignStats(region, response.pupil_cost, response.inmate_cost)
       })
   });
@@ -36,10 +37,6 @@ Map.prototype.assignStats = function(state, pupil_cost, inmate_cost){
 }
 
 
-
-var State = function(){
-}
-
 $(document).ready(function() {
     $('#close').hide();
     $('.stats').hide();
@@ -50,4 +47,49 @@ $(document).ready(function() {
       $('#close').hide();
     $('.stats').hide();
     })
+
+  
+  // var data_array = [4, 8, 15, 16, 23, 42];
+
+  // var w = 20,
+  //   h = 80;
+
+  // var x = d3.scale.linear()
+  //   .domain([0, 1])
+  //   .range([0, w]);
+
+  // var y = d3.scale.linear()
+  //   .domain([0, 100])
+  //   .rangeRound([0,h]);
+
+  // var chart = d3.select("body").append("svg")
+  //   .attr("class", "chart")
+  //   .attr("width", w * data_array.length - 1)
+  //   .attr("height", h);
+
+  // var bars = chart.selectAll("rect")
+  //   .data(data_array)
+  //   .enter().append("rect")
+  //   .attr("x", function(d, i) { return x(i) - .5; })
+  //   .attr("y", 80)
+  //   .attr("width", w)
+  //   .attr("height", y );
+
+  //   $('button').on("click", function() {
+  //   bars.transition().attr("y",function(d) { return h - y(d) - .5; });
+  // })
+
+  // chart.append("line")
+  //    .attr("x1", 0)
+  //    .attr("x2", w * data_array.length)
+  //    .attr("y1", h - .5)
+  //    .attr("y2", h - .5)
+  //    .style("stroke", "black");
   });
+
+
+
+
+
+
+
--- db/migrate/20130925234149_create_states.rb
+++ db/migrate/20130925234149_create_states.rb
@@ -2,10 +2,13 @@ class CreateStates < ActiveRecord::Migration
   def change
     create_table :states do |t|
       t.string :name
-      t.integer :education_per_capita
-      t.integer :incarceration_per_capita
+      t.integer :edu_per_capita
+      t.integer :inc_per_capita
 
       t.timestamps
     end
   end
+  # add_index :states, :name
+  # add_index :states, :edu_per_capita
+  # add_index :states, :inc_per_capita
 end
