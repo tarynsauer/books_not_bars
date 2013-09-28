61174d726d2ff9e38d8abe749432fb3674425764
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-28 18:21:29 -0500

    Refactored code to add legislators_control_info method

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -3,21 +3,12 @@ class RepresentativesController < ApplicationController
 
   def index
     @response = Sunlight_Foundation.response(params[:latitude], params[:longitude])
-    @response = nil
-    if @response
-      state = State.where(name: @response["results"].first["state_name"]).first
-      @message = "It's not OK that " + state.name + " spends $" + state.inc_per_capita.to_s + " to incarcerate an inmate per year and only $" + state.edu_per_capita.to_s + " to educate each pupil."
-      @form_message = @message + " Please work for prison and education spending reform. I support BooksNotBars."
-    end
+    display_legislators_info(@response)
   end
 
   def zipcode
     @response = Sunlight_Foundation.response_by_zipcode(params[:zipcode])
-    if @response
-      state = State.where(name: @response["results"].first["state_name"]).first
-      @message = "It's not OK that " + state.name + " spends $" + state.inc_per_capita.to_s + " to incarcerate an inmate per year and only $" + state.edu_per_capita.to_s + " to educate each pupil."
-      @form_message = @message + " Please work for prison and education spending reform. I support BooksNotBars."
-    end
+    display_legislators_info(@response)
   end
 
 end
