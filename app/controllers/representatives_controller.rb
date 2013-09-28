cb3388d999c5a1613cfe2ab3d825c324a9277dab
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-28 10:26:44 -0500

    Add dynamic tweet message

--- app/controllers/representatives_controller.rb
+++ app/controllers/representatives_controller.rb
@@ -4,6 +4,8 @@ class RepresentativesController < ApplicationController
   def index
     @response = Sunlight_Foundation.response(params[:latitude], params[:longitude])
     # @response = nil
+    state = State.where(name: @response["results"].first["state_name"]).first
+    @message = "It's not OK that " + state.name + " spends $" + state.inc_per_capita.to_s + " to incarcerate each inmate per year and only $" + state.edu_per_capita.to_s + " to educate each pupil."
   end
 
   def zipcode
