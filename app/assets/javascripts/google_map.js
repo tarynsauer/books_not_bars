f9c3139f660878bc43a2c0a1d93cac0d76dc95e2
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-29 13:49:43 -0500

    Add geolocation to Google map

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,22 +1,52 @@
 var getOrgsMap = function() {
+  google.maps.visualRefresh = true;
 
   function initialize() {
     var mapOptions = {
-      zoom: 4,
-      center: new google.maps.LatLng(-33, 151),
+      zoom: 6,
       mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
+    };
     map = new google.maps.Map(document.getElementById('map-canvas'),
-                                  mapOptions);
+        mapOptions);
 
-    // var image = 'images/beachflag.png';
-    // var myLatLng = new google.maps.LatLng(-33.890542, 151.274856);
-    // var beachMarker = new google.maps.Marker({
-    //     position: myLatLng,
-    //     map: map,
-    //     icon: image
-    // });
+    // Try HTML5 geolocation
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = new google.maps.LatLng(position.coords.latitude,
+                                         position.coords.longitude);
 
+        // var infowindow = new google.maps.InfoWindow({
+        //   map: map,
+        //   position: pos,
+        //   content: 'Location found using HTML5.'
+        // });
+
+        map.setCenter(pos);
+      }, function() {
+        handleNoGeolocation(true);
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleNoGeolocation(false);
+    }
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag) {
+      var content = 'Error: The Geolocation service failed.';
+    } else {
+      var content = 'Error: Your browser doesn\'t support geolocation.';
+    }
+
+    var options = {
+      map: map,
+      // Center of continental U.S.A.
+      position: new google.maps.LatLng(39.8282, 98.5795),
+      content: content
+    };
+
+    var infowindow = new google.maps.InfoWindow(options);
+    map.setCenter(options.position);
   }
 
   google.maps.event.addDomListener(window, 'load', initialize);
