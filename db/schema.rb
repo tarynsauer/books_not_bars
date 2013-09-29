ac832cd92926e6e3e8a02f66b76ef2a0e550b444
dbc-apprentice <apprentice@dbc-chicago-008.local>
2013-09-29 14:25:39 -0500

    no more sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130925234149) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "states", force: true do |t|
     t.string   "name"
     t.integer  "edu_per_capita"
