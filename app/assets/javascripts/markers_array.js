390b30939d306bfb26c3e0d3a979d1ba26846118
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-30 10:05:23 -0500

    Add basic styles to info windows

--- app/assets/javascripts/markers_array.js
+++ app/assets/javascripts/markers_array.js
@@ -3,7 +3,7 @@ var markers = [
 '<div id="siteNotice">'+
 '</div>'+
 '<h1 id="firstHeading" class="firstHeading">BUILD</h1>'+
-'<div id="bodyContent">'+
+'<div id="infoWindow">'+
 '<p><b>1443 W Division St</b></p>'+
 '<p>Chicago, IL 60642</p>'+
 '<p>BUILD’s mission is to engage at-risk youth in the schools and on the streets, so they can realize their educational and career potential and contribute to the stability, safety and well being of our communities. </p>'+
@@ -15,7 +15,7 @@ var markers = [
 '<div id="siteNotice">'+
 '</div>'+
 '<h1 id="firstHeading" class="firstHeading">PEAK</h1>'+
-'<div id="bodyContent">'+
+'<div id="infoWindow">'+
 '<p><b>1443 W Division St</b></p>'+
 '<p>Chicago, IL 60642</p>'+
 '<p>Partnershipt to Educate and Advance Kids (PEAK) is is a scholarship and mentoring program serving Chicago’s at-risk high school youth. We provide our students with a fully funded scholarship to attend Holy Trinity Catholic High School, totaling approximately $28,000 over the course of 4 years. Students in the program also work with a one-on-one gender-based mentor during their high school career.</p>'+
