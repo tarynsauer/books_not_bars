b61e9c2144bf7951cf62b18aaae08efb9f0cd0d0
Taryn Sauer <taryn.sauer@gmail.com>
2013-09-30 09:56:02 -0500

    Moved markers array to sepearate javascript file

---
+++ app/assets/javascripts/markers_array.js
@@ -0,0 +1,25 @@
+var markers = [
+{ 'name': 'Logan Square', 'location': [41.912945, -87.642746], 'description': '<div id="content">'+
+'<div id="siteNotice">'+
+'</div>'+
+'<h1 id="firstHeading" class="firstHeading">BUILD</h1>'+
+'<div id="bodyContent">'+
+'<p><b>1443 W Division St</b></p>'+
+'<p>Chicago, IL 60642</p>'+
+'<p>BUILD’s mission is to engage at-risk youth in the schools and on the streets, so they can realize their educational and career potential and contribute to the stability, safety and well being of our communities. </p>'+
+'<p>now serves over 3,500 youth annually in 11 Chicago communities: West Town, Humboldt Park, Cabrini Green, Logan Square, Hermosa, Belmont Cragin, East Garfield Park, North Lawndale, Englewood, Brighton Park and Austin. BUILD also continues to serve teens from the Cook County Juvenile Temporary Detention Center (CCJTDC), helping them successfully reintegrate into their home communities and the public school system and stay away from crime and the court system. </p>'+
+'<p>For more information, visit <a href="http://www.peakchicago.org/" target="_blank">http://www.buildchicago.org/g</a>.</p>'+
+'</div>'+
+'</div>'},
+{ 'name': 'Loop', 'location': [41.85569, -87.626266], 'description': '<div id="content">'+
+'<div id="siteNotice">'+
+'</div>'+
+'<h1 id="firstHeading" class="firstHeading">PEAK</h1>'+
+'<div id="bodyContent">'+
+'<p><b>1443 W Division St</b></p>'+
+'<p>Chicago, IL 60642</p>'+
+'<p>Partnershipt to Educate and Advance Kids (PEAK) is is a scholarship and mentoring program serving Chicago’s at-risk high school youth. We provide our students with a fully funded scholarship to attend Holy Trinity Catholic High School, totaling approximately $28,000 over the course of 4 years. Students in the program also work with a one-on-one gender-based mentor during their high school career.</p>'+
+'<p>For more information, visit <a href="http://www.peakchicago.org/" target="_blank">www.peakchicago.org</a>.</p>'+
+'</div>'+
+'</div>'}
+];
