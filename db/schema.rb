4a98284b5d58ecf18c1c1c94096a1e3d464ae479
Josh Suich <jsuich@gmail.com>
2013-09-30 10:49:43 -0500

    updating toolkit fix

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20130925234149) do
     t.integer  "edu_per_capita"
     t.integer  "inc_per_capita"
     t.integer  "whites_in_prison_per100k"
-    t.integer  "blacks_in_prison_per100k"
     t.integer  "latinos_in_prison_per100k"
+    t.integer  "blacks_in_prison_per100k"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
