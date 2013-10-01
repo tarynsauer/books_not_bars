1ef02f866ae2437b41fdb367d3e035d3514936d0
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-01 14:15:45 -0500

    fine tuning some styles, moved basic map function back to home

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -8,16 +8,9 @@ class MapController < ApplicationController
   end
 
   def update
-    p "xxxx-xxxxx"*200
+        
     p state_obj = State.where(name: params[:state])
 
-    # if state_obj.first.edu_per_capita
-    #   pupil_cost = state_obj.first.edu_per_capita
-    # end
-
-    # if state_obj.first.inc_per_capita
-    #   inmate_cost = state_obj.first.inc_per_capita
-    # end
 
     my_obj = state_obj[0]
     if request.xhr?
