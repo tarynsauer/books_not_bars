daa007e47dcd93d76ace587c1ea91ceef70e7e86
Taryn Sauer <taryn.sauer@gmail.com>
2013-10-02 16:49:48 -0500

    Seeded database with legislator data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131001222623) do
+ActiveRecord::Schema.define(version: 20131002234149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,39 @@ ActiveRecord::Schema.define(version: 20131001222623) do
     t.string   "twitter_handle"
     t.float    "latitude"
     t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "representatives", force: true do |t|
+    t.string   "title"
+    t.string   "firstname"
+    t.string   "middlename"
+    t.string   "lastname"
+    t.string   "name_suffix"
+    t.string   "nickname"
+    t.string   "party"
+    t.string   "state"
+    t.string   "district"
+    t.string   "in_office"
+    t.string   "gender"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "website"
+    t.string   "webform"
+    t.string   "congress_office"
+    t.string   "bioguide_id"
+    t.string   "votesmart_id"
+    t.string   "fec_id"
+    t.string   "govtrack_id"
+    t.string   "crp_id"
+    t.string   "twitter_id"
+    t.string   "congresspedia_url"
+    t.string   "youtube_url"
+    t.string   "facebook_id"
+    t.string   "official_rss"
+    t.string   "senate_class"
+    t.string   "birthdate"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
