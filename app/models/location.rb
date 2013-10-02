df91445422527230675a7af73735796526e4a5ad
Taryn Sauer <taryn.sauer@gmail.com>
2013-10-02 09:55:03 -0500

    Update Location model with geocoder callbacks

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   validates_format_of :website, :with => URI::regexp(%w(http https))
   validates_presence_of :title
+
+  geocoded_by :full_address  # can also be an IP address
+  after_validation :geocode  # auto-fetch coordinates
+
 end
